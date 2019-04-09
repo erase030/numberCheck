cfffbe0c78bf878a0a4f1db0bcb29798b43d901b
dit08 <dit@imac-3.local>
2019-04-09 16:06:52 +0900

    소수판별

--- numberCheck/ViewController.swift
+++ numberCheck/ViewController.swift
@@ -10,11 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblR: UILabel!
+    @IBOutlet weak var text: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        text.delegate = self as? UITextFieldDelegate
+        text.placeholder = "ex) 7"
+        text.clearButtonMode = UITextField.ViewMode.always
     }
 
-
+    
+    @IBAction func btnR(_ sender: Any) {
+        if let number = Int(text.text!) {
+            var isPrime = true
+            
+            if number <= 0 {
+                lblR.text = "input correct value!"
+                text.text = ""
+                return
+            }
+            
+            if number == 1 {
+                isPrime = false
+            }
+            
+            if number != 1 && number != 2 {
+                for i in 2 ..< number {
+                    if number % i == 0 {
+                        isPrime = false
+                    }
+                }
+            }
+            
+            if isPrime == true {
+                lblR.text = "prime number"
+            } else {
+                lblR.text = "Not Prime number"
+            }
+            text.text = ""
+        } else {
+            print("nil 발생")
+            lblR.text = "input numeric value!"
+            text.text = ""
+        }
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        text.resignFirstResponder()
+        return true
+    }
+    
 }
 
